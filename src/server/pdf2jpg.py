d3e92936aa158757f03b4e38f2532a3bbc345eb4
6Leoo6 <77102589+6Leoo6@users.noreply.github.com>
2022-10-06 20:51:00 +0200

    Possible path compatibility fix + 2 modules added

--- src/server/pdf2jpg.py
+++ src/server/pdf2jpg.py
@@ -1,36 +1,40 @@
 from shutil import rmtree
 from os import mkdir
 from zipfile import ZipFile
-
+from pathlib import Path
 import fitz
 
 
 async def convertPDF(file):
-    #Reseting the temp folder
-    rmtree('./server/temp/')
-    mkdir('./server/temp/')
+    #Defining the absolute path for future use
+    absolute_path = Path(__file__).parent.parent.absolute()
+    temp_path = absolute_path / 'server' / 'temp'
+    
+    # Reseting the temp folder
+    rmtree(temp_path)
+    mkdir(temp_path)
 
-    #Defineing basic names
+    # Defineing basic names
     name = file.filename[:-4]
-    pdf_name = f'./server/temp/{name}.pdf'
-    zip_name = f'./server/temp/{name}.zip'
+    pdf_name = temp_path / f'{name}.pdf'
+    zip_name = temp_path / f'{name}.zip'
 
-    #Writing the pdf into a temporary file so we can open it later
+    # Writing the pdf into a temporary file so we can open it later
     with open(pdf_name, 'wb') as pdf:
         pdf.write(await file.read())
 
-    #Openin the pdf file
+    # Openin the pdf file
     doc = fitz.open(pdf_name)
     with ZipFile(zip_name, 'w') as zipObj:
-        #Looping through the pages of the pdf
+        # Looping through the pages of the pdf
         for i in range(doc.page_count):
-            #Saving the pages as jpeg files
+            # Saving the pages as jpeg files
             doc.load_page(i).get_pixmap().save(
-                f'./server/temp/{name}_{str(i)}.jpeg')
-            #Adding the pages to a zip archive
+                temp_path / f'{name}_{str(i)}.jpeg')
+            # Adding the pages to a zip archive
             zipObj.write(
-                f'./server/temp/{name}_{str(i)}.jpeg', f'{name}_{str(i)}.jpeg')
-    #Getting the zip in a byte form
+                temp_path / f'{name}_{str(i)}.jpeg', f'{name}_{str(i)}.jpeg')
+    # Getting the zip in a byte form
     with open(zip_name, 'rb') as z:
         zip_bytes = z.read()
 
